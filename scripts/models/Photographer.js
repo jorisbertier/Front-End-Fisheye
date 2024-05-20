cd74e996ac369a085c5189ba12ceb28b1572001f
Joris Bertier <joris.bertier@gmail.com>
2024-05-20 08:40:08 -0500

    get json data photographers

---
+++ scripts/models/Photographer.js
@@ -0,0 +1,39 @@
+class photographers {
+    constructor(data) {
+        this.id  = data.id,
+        this.name = data.name,
+        this.city = data.city,
+        this.country = data.country,
+        this.tagline = data.tagline,
+        this.price = data.price,
+        this.portrait = data.portrait
+    }
+
+    get name() {
+        return this.name
+    }
+
+    get id() {
+        return this.id
+    }
+
+    get city() {
+        return this.city
+    }
+
+    get country() {
+        return this.country
+    }
+
+    get tagline() {
+        return this.tagline
+    }
+
+    get price() {
+        return this.price
+    }
+
+    get portrait() {
+        return this.portrait
+    }
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,3 +1,6 @@
+
+
+
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
