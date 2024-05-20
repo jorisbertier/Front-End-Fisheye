5f393661a93b1259bc9c0c829fea8950c4037e20
Joris Bertier <joris.bertier@gmail.com>
2024-05-20 10:14:32 -0500

    style photographer.html

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -13,11 +13,14 @@ function photographerTemplate(data) {
         const h3 = document.createElement('h3')
         const p = document.createElement('p')
         const span = document.createElement('span')
-        const alt = document.createElement('alt')
+        const div = document.createElement('div')
+        const a = document.createElement('a')
 
         img.setAttribute("src", picture)
         img.setAttribute("alt", `Profil picture ${name}`)
+        a.setAttribute('href', 'photographer.html')
         article.classList.add('wrapper__photographers')
+        a.classList.add('wrapper__photographers--link')
         img.classList.add('wrapper__photographers--img')
         h2.classList.add('wrapper__photographers--title')
         h3.classList.add('wrapper__photographers--second-title')
@@ -25,13 +28,14 @@ function photographerTemplate(data) {
         span.classList.add('wrapper__photographers--span')
 
 
+
         h2.textContent = name;
         h3.innerText = `${city}, ${country}`
         p.innerText = `${tagline}`
         span.innerText = `${price}€/jour`
-
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(a)
+            a.appendChild(img)
+            a.appendChild(h2)
         article.appendChild(h3)
         article.appendChild(p)
         article.appendChild(span)
