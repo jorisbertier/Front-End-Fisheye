e691abe55dd3c81814a77f0173a89881cbc9cd1d
Joris Bertier <joris.bertier@gmail.com>
2024-06-05 15:46:09 -0500

    add style trier

---
+++ scripts/functions/openDropdown.js
@@ -0,0 +1,16 @@
+let openDropdown = document.querySelector('.open__dropdown-menu')
+let list = document.querySelector('.dropdown-menu__list')
+let closeDropdown = document.querySelector('.fa-chevron-up')
+console.log(openDropdown)
+console.log(list)
+
+openDropdown.addEventListener('click', ()=> {
+    list.classList.add('open')
+    openDropdown.classList.add('close')
+})
+
+closeDropdown.addEventListener('click', ()=> {
+    list.classList.remove('open')
+    list.classList.add('close')
+    openDropdown.classList.remove('close')
+})
