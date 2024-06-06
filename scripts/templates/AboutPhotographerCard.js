5e72e81e9e5ba05fb614d428491580ca5ee4e071
Joris Bertier <joris.bertier@gmail.com>
2024-06-06 14:45:05 -0500

    accesibility aboutPhotographer

--- scripts/templates/AboutPhotographerCard.js
+++ scripts/templates/AboutPhotographerCard.js
@@ -7,13 +7,13 @@ class AboutPhotographerCard {
         const { name, portrait, city, country, tagline } = this.data;
         let template = document.createElement('template');
         template.innerHTML = `
-        <div class="main__content">
+        <div class="main__content" aria-labelledby="photographer-name">
             <h1 class="main__content--title">${name}</h1>
             <h2 class="main__content--subtitle">${city}, ${country}</h2>
             <h3 class="main__content--subtitle-text">${tagline}</h3>
         </div>
         <div class="photograph-header main__modal">
-            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+            <button class="contact_button" onclick="displayModal()"  aria-label="Ouvrir le formulaire de contact de ${name}">Contactez-moi</button>
         </div>
         <div class="main__picture">
             <img src="/assets/photographers/${portrait}" alt="Profile picture ${name}" class="main__picture--img"/>
