4b79bfebd5eb9bbc239d64dfcb97fe7a1015ec9a
Joris Bertier <joris.bertier@gmail.com>
2024-06-09 08:16:45 -0500

    style carrousel

--- scripts/templates/LightBoxCard.js
+++ scripts/templates/LightBoxCard.js
@@ -26,18 +26,18 @@ class LightBox {
         template.innerHTML = `
             <div class="lightbox-content">
                 <div class="lightbox-navigation lightbox-navigation-left">
-                    <span class="lightbox-arrow">&lt;</span>
+                    <span class="lightbox-arrow"><i class="fa-solid fa-angle-left"></i></span>
                 </div>
                 <div class="lightbox-media-container">
-                    <img src="" class="lightbox-media" alt="Description de votre image">
-                    <video class="rounded wrapper__media--video hidden" controls tabindex="-1" width="100%" height="100%" aria-label="">
+                    <img src="" class="lightbox-media" alt="${title}">
+                    <video class="rounded wrapper__media--video hidden" controls tabindex="-1" width="100%" height="100%" alt="${title}" aria-label="">
                         <source src="" class="video__source"type="video/mp4">
                     </video>
                 </div>
                 <div class="lightbox-navigation lightbox-navigation-right">
-                    <span class="lightbox-arrow">&gt;</span>
+                    <span class="lightbox-arrow"><i class="fa-solid fa-angle-right"></i></span>
                 </div>
-                <span class="lightbox-close">&times;</span>
+                <span class="lightbox-close"><i class="fa-solid fa-xmark"></i></span>
             </div>
         `;
         return template.content.cloneNode(true);
