68344bcb5c42cdc7f30bb19fe1d841a1f9bc18aa
Joris Bertier <joris.bertier@gmail.com>
2024-06-11 11:35:14 -0500

    add accesibility index

--- scripts/templates/AboutPhotographerCard.js
+++ scripts/templates/AboutPhotographerCard.js
@@ -7,7 +7,7 @@ class AboutPhotographerCard {
         const { name, portrait, city, country, tagline } = this.data;
         let template = document.createElement('template');
         template.innerHTML = `
-        <div class="main__content" aria-labelledby="photographer-informations">
+        <div class="main__content" aria-labelledby="photographer-informations" tabindex="0">
             <h1 class="main__content--title">${name}</h1>
             <h2 class="main__content--subtitle">${city}, ${country}</h2>
             <h3 class="main__content--subtitle-text">${tagline}</h3>
@@ -15,8 +15,8 @@ class AboutPhotographerCard {
         <div class="photograph-header main__modal">
             <button class="contact_button" onclick="displayModal()"  aria-label="Ouvrir le formulaire de contact de ${name}">Contactez-moi</button>
         </div>
-        <div class="main__picture">
-            <img src="/assets/photographers/${portrait}" alt="Profile picture ${name}" class="main__picture--img"/>
+        <div class="main__picture" tabindex="0">
+            <img src="/assets/photographers/${portrait}" alt="Image de profile de ${name}" class="main__picture--img"/>
         </div>
         `
         return template.content.cloneNode(true);
--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -12,14 +12,14 @@ class PhotographerCard {
         const template = document.createElement('template');
         template.innerHTML =
         `
-        <article class="wrapper__photographers" role="article" aria-labelledby="photographer-${id}-name">
-            <a href="photographer.html?id=${id}" class="wrapper__photographers--link" aria-label="Ouvrir le carrousel à partir de l'article ${name}">
-                <img src="${picture}" alt="Image profil de ${name}" aria-label="Profil de ${name}" class="wrapper__photographers--img"/>
+        <article class="wrapper__photographers" role="article" aria-label="Article de ${name}" aria-labelledby="photographer-${id}-name">
+            <a href="photographer.html?id=${id}" class="wrapper__photographers--link" aria-label="Ouvrir la page d'information de ${name} à partir de cette l'article" tabindex="0">
+                <img src="${picture}" alt="Image profil de ${name}" aria-label="Image de profile de ${name}" class="wrapper__photographers--img"/>
                 <h2 class="wrapper__photographers--title">${name}</h2>
             </a>
-            <h3 class="wrapper__photographers--second-title">${city}, ${country}</h3>
-            <p class="wrapper__photographers--content">${tagline}</p>
-            <span class="wrapper__photographers--span">${price}€/jour</span>
+            <h3 class="wrapper__photographers--second-title" aria-label="Ville et pays de ${name} ${city}, ${country}" tabindex="0">${city}, ${country}</h3>
+            <p class="wrapper__photographers--content" aria-label="Expression favorite de ${name}, ${tagline}" tabindex="0">${tagline}</p>
+            <span class="wrapper__photographers--span" aria-label="Prix de ${price} euros par jour pour ${name}" tabindex="0">${price}€/jour</span>
         </article>
         `;
         return template.content.cloneNode(true);
